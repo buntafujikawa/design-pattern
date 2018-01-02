0e9ea94fc4c899a51865d0b79af32c6a9e163a95
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-01-02 15:58:24 +0900

    add bridge pattern

---
+++ bridge/Display.java
@@ -0,0 +1,26 @@
+// 表示するクラス(機能のクラス階層の最上位)
+public class Display {
+    private DisplayImpl impl;
+
+    public Display(DisplayImpl impl) {
+        this.impl = impl;
+    }
+
+    public void open() {
+        impl.rawOpen();
+    }
+
+    public void print() {
+        impl.rawPrint();
+    }
+
+    public void close() {
+        impl.rawClose();
+    }
+
+    public final void display() {
+        open();
+        print();
+        close();
+    }
+}
