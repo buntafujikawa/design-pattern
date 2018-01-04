09fce40f427b6251ce9700f77e9a020f2658cf5b
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-01-04 23:06:13 +0900

    add singleton pattern

---
+++ singleton/Main.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start.");
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        if (obj1 == obj2) {
+            System.out.println("同じ");
+        } else {
+            System.out.println("違う");
+        }
+
+        System.out.println("End.");
+    }
+}
---
+++ singleton/README.md
@@ -0,0 +1,8 @@
+# Singletonパターン
+インスタンスが1個しか存在しないことを保証するデザインパターン
+
+## 登場人物
+- Singletonの役  
+
+## クラス図
+![singleton](https://user-images.githubusercontent.com/11749585/34566703-e6a349b8-f1a2-11e7-8785-49f2c55f53db.jpg)
