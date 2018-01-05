eb64c88a2e1bd4a0c6e1e53e9c209775aaa208be
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-01-05 19:48:17 +0900

    add builder pattern

---
+++ builder/HTMLBuilder.java
@@ -0,0 +1,29 @@
+package builder;
+
+public class HTMLBuilder extends Builder {
+
+    @Override
+    public void makeTitle(String title) {
+        // to leave out
+    }
+
+    @Override
+    public void makeString(String str) {
+        // to leave out
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        // to leave out
+    }
+
+    @Override
+    public void close() {
+        // to leave out
+    }
+
+    public String getResult() {
+        // to leave out
+        return "result";
+    }
+}
---
+++ builder/README.md
@@ -0,0 +1,15 @@
+# Prototypeパターン
+構造を持ったインスタンスを組み上げていくデザインパターン    
+
+## 登場人物
+- Builder(建築者)の役  
+Builderクラス
+- ConcreateBuilder(具体的建築者)の役  
+TextBuilderクラス、HTMLBuilderクラス
+- Director(監督者)の役  
+Directorクラス
+- Client(依頼人)の役  
+Mainクラス
+
+## クラス図
+![builder](https://user-images.githubusercontent.com/11749585/34570095-259817ec-f1ae-11e7-990b-392d4a34b77b.jpg)
